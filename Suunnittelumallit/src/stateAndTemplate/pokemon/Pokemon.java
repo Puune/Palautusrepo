6c8a9b5d8b7e60be17f9c99b567f89ea5b488fda
Panu Lindqvist <43564655+Puune@users.noreply.github.com>
2020-04-01 14:10:32 +0300

    comment

--- Suunnittelumallit/src/stateAndTemplate/pokemon/Pokemon.java
+++ Suunnittelumallit/src/stateAndTemplate/pokemon/Pokemon.java
@@ -14,6 +14,7 @@ public class Pokemon {
 	/**
 	 * Create new pokemon
 	 * @param state {@link EvolutionState}
+	 * @param name String
 	 */
 	public Pokemon(EvolutionState intitialState, String name) {
 		this.state = intitialState;
